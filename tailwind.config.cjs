33d1810784c25668bc6cf4f1ac33ba5287592868
Pedro Verani <pedropverani@hotmail.com>
2024-07-28 21:38:12 -0300

    improving

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      './src/**/*.{html,js,svelte,ts}'
-    ],
+    purge: ['./src/**/*.{svelte,js,ts}'],
+    darkMode: false, // or 'media' or 'class'
     theme: {
       extend: {},
     },
+    variants: {
+      extend: {},
+    },
     plugins: [],
-  }
+  };
   
